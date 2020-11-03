b355ee62fa540b46cf020262286d7738d162055a
Kervintz Noel <kervintz84@hotmail.com>
2020-11-02 23:04:06 -0500

    Updates

---
+++ precache-manifest.52de66015a1c7082315da0518b47bbb0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b024d55c3ad9520f96d52218cf832fb9",
+    "url": "/react-gallery-app/index.html"
+  },
+  {
+    "revision": "6a91ceb131a60b4b3b20",
+    "url": "/react-gallery-app/static/css/main.8325ea2a.chunk.css"
+  },
+  {
+    "revision": "958e08637e3fd7663bcc",
+    "url": "/react-gallery-app/static/js/2.3d657918.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-gallery-app/static/js/2.3d657918.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a91ceb131a60b4b3b20",
+    "url": "/react-gallery-app/static/js/main.168b61b6.chunk.js"
+  },
+  {
+    "revision": "26f21656774d221cea29",
+    "url": "/react-gallery-app/static/js/runtime-main.c13417b7.js"
+  }
+]);
